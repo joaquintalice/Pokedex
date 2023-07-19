5fe230297f31b7af5467e67c6d7c9464a8ae3b65
Joaquin Talice <joaquintalice@gmail.com>
2023-07-19 18:39:22 -0300

    Envrionment update

--- src/config/joi.schema.validation.ts
+++ src/config/joi.schema.validation.ts
@@ -2,6 +2,6 @@ import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
     MONGODB: Joi.required(),
-    PORT: Joi.number().default(3005),
-    DEFAULT_LIMIT: Joi.number().default(6)
+    PORT: Joi.number().default(3000),
+    DEFAULT_LIMIT: Joi.number().default(5)
 })
